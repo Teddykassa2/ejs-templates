9c21548f3a14d155205ce5e59ae449da45cec940
Teddy Kassa <103133862+Teddykassa2@users.noreply.github.com>
2022-08-16 17:33:55 +0000

    update

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+//this will allow us to serve up static files, CSS, images & JS
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+ 
+  var title = 'Our Home Page';
+  var heading = 'My Website';
+  res.render('pages/index',{
+    'title':title,
+    'heading': heading
+  });
+})
+
+app.get('/m1max', (req, res) => {
+ 
+  var title = 'Apple M1 Max Page';
+  var heading = 'My Website';
+  res.render('pages/m1max',{
+    'title':title,
+    'heading': heading
+  });
+})
+
+app.get('/cars', (req, res) => {
+ 
+  var title = 'Corvette C8 Page';
+  var heading = 'My Website';
+  res.render('pages/cars',{
+    'title':title,
+    'heading': heading
+  });
+})
+
+app.get('/movies', (req, res) => {
+ 
+  var title = 'MCU Movies Page';
+  var heading = 'My Website';
+  res.render('pages/movies',{
+    'title':title,
+    'heading': heading
+  });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
